3ed8cd45c22e0ff7a26b399b63726d38f004e183
Jimmy Chu <rjchu@uwaterloo.ca>
2020-10-18 23:02:07 -0400

    project finished?

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,60 @@
+class StudentsController < ApplicationController 
+
+  skip_before_action :require_user, only: [:new, :create]
+  before_action :require_same_student, only: [:edit, :update ]
+  before_action :set_student, only: [:show, :edit, :update]
+
+
+  def index
+    @students = Student.all
+  end 
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.save
+      flash[:notice] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end 
+
+  def show 
+    @student = Student.find(params[:id])
+  end 
+
+  def edit 
+    @student = Student.find(params[:id])
+  end 
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to @student
+    else
+      render 'edit'
+    end 
+  end 
+
+  private
+  
+  def set_student
+    @student = Student.find(params[:id])
+  end 
+
+  def student_params
+    params.require(:student).permit(:name, :email, :password)
+  end 
+
+  def require_same_student
+    if current_user != @student
+      redirect_to student_path(current_user)
+      flash[:notice] = "You can only edit your own profile"
+    end 
+  end  
+end
